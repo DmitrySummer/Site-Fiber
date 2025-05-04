f581364f3df510c47ea253d4a4c719e5908cbd35
DmitrySummer <dielsunsummer@yahoo.com>
2025-05-04 13:19:47 +0300

    Revert "Logger-3"

--- cmd/main.go
+++ cmd/main.go
@@ -4,9 +4,10 @@ import (
 	"dl/new-web-site/config"
 	"dl/new-web-site/internal/pages"
 	"dl/new-web-site/pkg/logger"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
-	slogfiber "github.com/samber/slog-fiber"
+	"github.com/gofiber/contrib/slogfiber"
 )
 
 func main() {
@@ -20,6 +21,22 @@ func main() {
 
 	// Конфигурация middleware для логирования
 	app.Use(slogfiber.New(log))
+
+	// Дополнительное логирование с помощью slog
+	app.Use(func(c *fiber.Ctx) error {
+		start := time.Now()
+		err := c.Next()
+		duration := time.Since(start)
+
+		log.Info("request completed",
+			"method", c.Method(),
+			"path", c.Path(),
+			"status", c.Response().StatusCode(),
+			"duration", duration,
+			"ip", c.IP(),
+		)
+		return err
+	})
 
 	pages.NewHandler(app)
 
