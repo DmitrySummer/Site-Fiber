2e4294f8db146f6a8ca3f55d8cdde970d93254f4
DmitrySummer <dielsunsummer@yahoo.com>
2025-05-04 13:06:05 +0300

    Logger-3

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,9 @@ import (
 	"dl/new-web-site/config"
 	"dl/new-web-site/internal/pages"
 	"dl/new-web-site/pkg/logger"
-	"time"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/contrib/slogfiber"
+	slogfiber "github.com/samber/slog-fiber"
 )
 
 func main() {
@@ -21,22 +20,6 @@ func main() {
 
 	// Конфигурация middleware для логирования
 	app.Use(slogfiber.New(log))
-
-	// Дополнительное логирование с помощью slog
-	app.Use(func(c *fiber.Ctx) error {
-		start := time.Now()
-		err := c.Next()
-		duration := time.Since(start)
-
-		log.Info("request completed",
-			"method", c.Method(),
-			"path", c.Path(),
-			"status", c.Response().StatusCode(),
-			"duration", duration,
-			"ip", c.IP(),
-		)
-		return err
-	})
 
 	pages.NewHandler(app)
 
