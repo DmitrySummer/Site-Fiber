d0970623a6f1a0d52d23e1c5d51dde4845eac585
DmitrySummer <dielsunsummer@yahoo.com>
2025-05-04 12:40:30 +0300

    Logger

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,34 @@
 package main
 
 import (
+	"dl/new-web-site/config"
 	"dl/new-web-site/internal/pages"
+	"dl/new-web-site/pkg/logger"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	config.Init()
+
+	// Создаём конфиг и логгер
+	logConfig := config.NewLogConfig()
+	log := logger.NewLogger(logConfig)
+
 	app := fiber.New()
+
+	app.Use(func(c *fiber.Ctx) error {
+		log.Info("incoming request",
+			"method", c.Method(),
+			"path", c.Path(),
+			"ip", c.IP(),
+		)
+		return c.Next()
+	})
 
 	pages.NewHandler(app)
 
-	app.Listen(":3000")
-
+	if err := app.Listen(":3000"); err != nil {
+		log.Error("failed to start server", "error", err)
+	}
 }
