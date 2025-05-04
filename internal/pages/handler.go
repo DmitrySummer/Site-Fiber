b069191f70c50302af98c66928460d73d5b3f90c
DmitrySummer <dielsunsummer@yahoo.com>
2025-05-04 13:10:49 +0300

    Logger-4

--- internal/pages/handler.go
+++ internal/pages/handler.go
@@ -1,16 +1,40 @@
 package pages
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"dl/new-web-site/config"
+	"dl/new-web-site/pkg/logger"
+
+	"github.com/gofiber/fiber/v2"
+	"log/slog"
+)
 
 type PagesHandler struct {
 	router fiber.Router
+	log    *slog.Logger
 }
 
 func NewHandler(router fiber.Router) {
+	// Создаём конфиг и логгер
+	logConfig := config.NewLogConfig()
+	log := logger.NewLogger(logConfig)
+
 	h := &PagesHandler{
 		router: router,
+		log:    log,
 	}
+
 	api := h.router.Group("/pages")
+
+	// Добавляем middleware для логирования
+	api.Use(func(c *fiber.Ctx) error {
+		h.log.Info("incoming request",
+			"method", c.Method(),
+			"path", c.Path(),
+			"ip", c.IP(),
+		)
+		return c.Next()
+	})
+
 	api.Get("/", h.pages)
 
 }
