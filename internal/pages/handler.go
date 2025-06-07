f10a5a73b9e3e899fcdd8688cda0f32fdebeaa09
DmitrySummer <dielsunsummer@yahoo.com>
2025-06-07 11:17:19 +0300

    Создание страницы с регистрацией

--- internal/pages/handler.go
+++ internal/pages/handler.go
@@ -20,10 +20,31 @@ func NewHandler(router fiber.Router, log *slog.Logger) *PagesHandler {
 	}
 
 	h.router.Get("/", h.pages)
+	h.router.Get("/register", h.registerGet)
+	h.router.Post("/register", h.registerPost)
 	return h
 }
 
 func (h *PagesHandler) pages(c *fiber.Ctx) error {
 	components := views.Main()
 	return tadaprot.Render(c, components)
+}
+
+func (h *PagesHandler) registerGet(c *fiber.Ctx) error {
+	h.log.Info("Открыта страница /register")
+	return tadaprot.Render(c, views.Register())
+}
+
+func (h *PagesHandler) registerPost(c *fiber.Ctx) error {
+	name := c.FormValue("name")
+	email := c.FormValue("email")
+	password := c.FormValue("password")
+
+	h.log.Info("Register form submitted",
+		"name", name,
+		"email", email,
+		"password", password,
+	)
+
+	return c.SendString("Регистрация прошла успешно!")
 }
